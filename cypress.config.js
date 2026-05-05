b9b9345df8bcc84c2c88e28e2d4aacca125df70b
Maycon <maycon_gr@hotmail.com>
2026-05-05 14:59:58 -0300

    Add link url

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
+     baseUrl: 'https://automationexercise.com',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+     
     },
   },
 });
